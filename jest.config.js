20edf6463f66db7a1f36b3ebba0b1dbe1fed9d79
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-10-11 21:45:03 +0900

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: [
+    "<rootDir>/src"
+  ],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json",
+    "node"
+  ],
+};
