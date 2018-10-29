b2e8f782fb2aff8cc6490f61b2da96e4adff7e5a
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-10-30 08:59:07 +0900

    feat: implement specify morning greeting

--- src/controllers/init.ts
+++ src/controllers/init.ts
@@ -1,14 +1,23 @@
+import { CronJob } from 'cron';
 import { SlackBot } from 'botkit';
 
 export const init: (err: string, bot: SlackBot) => void = (err, bot) => {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
-  bot.say({
-    channel: 'front-end',
-    text:
-      '<!here> Good morning :sunny: \n Please tell me the progress ? :eyes:',
-    username: 'sushi-kun',
-    icon_url: 'https://ca.slack-edge.com/T30J6REKB-UDF5GG831-b05e0e33b5d8-72',
+  const cronTime = '0 15 9 * * *';
+  const onTick = () =>
+    bot.say({
+      channel: 'front-end',
+      text:
+        '<!here> Good morning :sunny: \n Please tell me the progress ? :eyes:',
+      username: 'sushi-kun',
+      icon_url: 'https://ca.slack-edge.com/T30J6REKB-UDF5GG831-b05e0e33b5d8-72',
+    });
+  new CronJob({
+    cronTime,
+    onTick,
+    start: true,
+    timeZone: 'Asia/Tokyo',
   });
 };
